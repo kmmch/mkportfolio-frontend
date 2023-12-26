20c66219b07448374ce77fe68201098012cc89a5
Masaya Kemmochi <82385253+kmmch@users.noreply.github.com>
2023-12-26 14:12:01 +0000

    Update 2 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
-import { Pra1 } from "./ practice/pra1";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>React + Vite</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      <Pra1 />
+      <h1>Hello, MyPortfolio!!</h1>
     </>
   );
 }
