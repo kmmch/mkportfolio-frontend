53b6462adad65a7d785a04a885d08a8e20e88c65
kmmch <kmmch.tech@gmail.com>
2023-12-27 18:01:52 +0900

    use params

---
+++ src/components/pages/Page.tsx
@@ -0,0 +1,10 @@
+import { FC, memo } from "react";
+import { useParams } from "react-router-dom";
+
+export const Page:FC = memo(() => {
+    // const params = useParams();
+
+    return (
+        <p>page</p>
+    );
+});
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import App from "../App";
 import { NotFound } from "../components/pages/NotFound";
+import { Page } from "../components/pages/Page";
 
 
 export const Router:FC = memo(() => {
     return (
         <Routes>
             <Route path="/" element={<App />} />
+            <Route path="/page/:id" element={<Page />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     );
