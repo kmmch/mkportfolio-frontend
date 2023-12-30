5ccc14ac24dfddb13b5bff0bd3ab15d529ee61b5
kmmch <kmmch.tech@gmail.com>
2023-12-30 16:41:17 +0900

    一旦ヘッダー隠す

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,13 +4,13 @@ import { FC, memo } from "react";
 export const Header:FC = memo(() => {
     return (
         <>
-            <Box py="0">
+            {/* <Box py="0" position="fixed" w="100%">
                 <Container>
                     <Center>
                         <Heading as="h1">m0chi.work</Heading>
                     </Center>
                 </Container>
-            </Box>
+            </Box> */}
         </>
     );
 });
