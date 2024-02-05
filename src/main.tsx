030ac5a666a1305de39c956cb17d759ebda9a964
kmmch <kmmch.tech@gmail.com>
2024-02-06 00:03:20 +0900

    setup chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/Router.tsx";
 import { BasicLayout } from "./components/templates/BasicLayout.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BasicLayout>
-            <BrowserRouter>
-                <Router />
-            </BrowserRouter>
-        </BasicLayout>
+        <ChakraProvider>
+            <BasicLayout>
+                <BrowserRouter>
+                    <Router />
+                </BrowserRouter>
+            </BasicLayout>
+        </ChakraProvider>
     </React.StrictMode>,
 );
