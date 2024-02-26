506777473c8dc4d4b51b9ec0f26385b2dc95336d
kmmch <kmmch.tech@gmail.com>
2024-02-26 22:35:13 +0900

    トッププロフィールをコンポーネント化

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,9 +3,6 @@ import {
     Box,
     Center,
     Flex,
-    Heading,
-    Image,
-    Link,
     Tab,
     TabList,
     TabPanels,
@@ -13,15 +10,13 @@ import {
     Text,
 } from "@chakra-ui/react";
 
-import { FaGithub } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-
 import { Section } from "../organisms/Section";
 import { LinkButton } from "../atoms/LinkButton";
 import { WorkCardList } from "../molecules/WorkCardList";
 import { SkillCardList } from "../molecules/SkillCardList";
 import { Level } from "../atoms/Level";
 import { BACK_SKILL_DATA, FRONT_SKILL_DATA, OTHER_SKILL_DATA } from "../../data/skills";
+import { TopProfile } from "../molecules/TopProfile";
 
 
 export const Home: FC = memo(() => {
@@ -30,36 +25,7 @@ export const Home: FC = memo(() => {
     return (
         <>
             <Section>
-                <Image
-                    src='./myicon.png'
-                    alt='Mochiken'
-                    borderRadius='full'
-                    boxSize='50%'
-                    m='auto'
-                />
-                <Center my='5'>
-                    <Link href='https://github.com/kmmch' target='_blank'>
-                        <FaGithub size="2rem" />
-                    </Link>
-                    <Link href='https://twitter.com/mochiken__code' target='_blank'>
-                        <FaXTwitter size="2rem" />
-                    </Link>
-                </Center>
-                
-
-                <Box my='5'>
-                    <Heading as='h2' textAlign='center'>Mochiken</Heading>
-                    <Text textAlign='center'>WEBエンジニア</Text>
-                </Box>
-
-
-                <Box my='5'>
-                    <Text mb='7.5px'>学生時代は自然言語処理を専攻。</Text>
-                    <Text mb='7.5px'>大学卒業後にIT企業へ入社し、主にCMSを使ったWEBサイト構築に従事。<br />要件定義から運用保守まで幅広い経験を持つ。</Text>
-                    <Text mb='7.5px'>DrupalなどCMSを使ったWEBサイト構築が得意。</Text>
-                    <Text mb='7.5px'>現在はモダンフレームワークを使ったWEBアプリ開発を勉強中。</Text>
-                </Box>
-
+                <TopProfile />
             </Section>
 
             <Section 
