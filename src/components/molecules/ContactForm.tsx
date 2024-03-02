8e32fde172622f0b37945f3a396b45fc98cd3ff1
kmmch <kmmch.tech@gmail.com>
2024-03-02 15:27:41 +0900

    自サイト内で問い合わせできるようにはした

--- src/components/molecules/ContactForm.tsx
+++ src/components/molecules/ContactForm.tsx
@@ -1,11 +1,80 @@
 import { FC, memo } from "react";
-
-// type Props = {};
+import { useForm } from 'react-hook-form'
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Textarea
+} from '@chakra-ui/react'
 
 export const ContactForm: FC = memo(() => {
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitting },
+    } = useForm()
+    
+    function onSubmit(values: any) {
+        return new Promise<void>((resolve) => {
+            setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                resolve();
+            }, 1500)
+        })
+    }
     return (
-        <>
-            <p>ContactForm</p>
-        </>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            {/* お名前 */}
+            <FormControl isInvalid={errors.name}>
+                <FormLabel htmlFor='name'>お名前</FormLabel>
+                <Input
+                    id='name'
+                    placeholder='問合　太郎'
+                    {...register('name', {
+                        required: 'お名前を入力してください',
+                    })}
+                />
+                <FormErrorMessage>
+                    {errors.name && errors.name.message}
+                </FormErrorMessage>
+            </FormControl>
+            {/* メールアドレス */}
+            <FormControl isInvalid={errors.email}>
+                <FormLabel htmlFor='email'>メールアドレス</FormLabel>
+                <Input
+                    id='email'
+                    placeholder='abcd1234@example.com'
+                    {...register('email', {
+                        required: 'メールアドレスを入力してください',
+                        pattern: {
+                            value: /\S+@\S+\.\S+/,
+                            message: "Entered value does not match email format",
+                        },
+                    })}
+                />
+                <FormErrorMessage>
+                    {errors.email && errors.email.message}
+                </FormErrorMessage>
+            </FormControl>
+            {/* お問い合わせ内容 */}
+            <FormControl isInvalid={errors.body}>
+                <FormLabel htmlFor='body'>お問い合わせ内容</FormLabel>
+                <Textarea
+                    id='body'
+                    placeholder='お問い合わせ内容を入力してください'
+                    {...register('body', {
+                        required: 'お問い合わせ内容を入力してください',
+                    })}
+                />
+                <FormErrorMessage>
+                    {errors.body && errors.body.message}
+                </FormErrorMessage>
+            </FormControl>
+            <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+                Submit
+            </Button>
+      </form>
     );
 });
