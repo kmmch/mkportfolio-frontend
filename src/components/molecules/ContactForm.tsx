d508737e5b8fe9383356cfc1a04f097bc1c1a605
kmmch <kmmch.tech@gmail.com>
2024-03-02 16:17:25 +0900

    本番で使用可能か確認する

--- src/components/molecules/ContactForm.tsx
+++ src/components/molecules/ContactForm.tsx
@@ -1,30 +1,48 @@
-import { FC, memo } from "react";
+import { FC, memo, useEffect } from "react";
 import { useForm } from 'react-hook-form'
 import {
-  FormErrorMessage,
-  FormLabel,
-  FormControl,
-  Input,
   Button,
   Textarea
 } from '@chakra-ui/react'
+
 import { ControlledInput } from "../atoms/ContorolledInput";
+import { useCsrfToken } from "../../hooks/useCsrfToken";
+import axios from "axios";
 
 export const ContactForm: FC = memo(() => {
+
+    // CSRFトークン取得
+    const { getCsrfToken, csrf_token } = useCsrfToken();
+    useEffect(() => getCsrfToken(), []);
+    console.log(csrf_token);
+
+    // お問合せを機能させるための準備
     const {
         handleSubmit,
         register,
         formState: { errors, isSubmitting },
-    } = useForm()
+    } = useForm();
     
+
+    // お問合せが送信された時の動作
     function onSubmit(values: any) {
+        const headers = {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': csrf_token
+        }
         return new Promise<void>((resolve) => {
-            setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
+            setTimeout(async () => {
+                // alert(JSON.stringify(values, null, 2));
+                const response = await axios.post(
+                    'https://api.mochiken.work/webform_rest/submit',
+                    values,
+                    {headers: headers});
                 resolve();
             }, 1500)
         })
     }
+
+    // お問合せのHTMLをレンダリング
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <ControlledInput
@@ -53,7 +71,6 @@ export const ContactForm: FC = memo(() => {
                     required: 'お問い合わせ内容を入力してください',
                 })}
             />
-
             <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
                 Submit
             </Button>
