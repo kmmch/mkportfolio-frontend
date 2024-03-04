1df9291a9a47852bb7ba82e86fa73fd3ec834c42
kmmch <kmmch.tech@gmail.com>
2024-03-04 20:44:12 +0900

    レイアウト変更

--- src/components/templates/BasicLayout.tsx
+++ src/components/templates/BasicLayout.tsx
@@ -12,20 +12,13 @@ export const BasicLayout: FC<Props> = memo((props) => {
 
     return (
         <>
-            <Box backgroundColor='blackAlpha.800' py={5} px={3}>
-                <Box backgroundColor='white' borderRadius={35}>
-                    <Header />
-
-                    <Box py={5}>
-                        <BasicContainer>
-                            { children }
-                        </BasicContainer>
-                    </Box>
-
-                    {/* <Footer /> */}
-                </Box>
-                <Footer />
+            <Header />
+            <Box py={5}>
+                <BasicContainer>
+                    { children }
+                </BasicContainer>
             </Box>
+            <Footer />
         </>
     );
 });
