696d6994bdfbac47c203af9fb1eacb86bcc4b78a
kmmch <kmmch.tech@gmail.com>
2024-03-05 20:06:22 +0900

    一旦アニメーションを無効化

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -15,12 +15,27 @@ import { SkillCardList } from "../molecules/SkillCardList";
 import { Level } from "../atoms/Level";
 import { TopProfile } from "../molecules/TopProfile";
 import { ContactForm } from "../molecules/ContactForm";
+// import { motion, useScroll } from "framer-motion";
 
 export const Home: FC = memo(() => {
 
+    // const { scrollYProgress } = useScroll();
 
     return (
         <>
+            {/* <motion.div
+                style={{
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    zIndex: 9,
+                    height: "20px",
+                    transformOrigin: "0%",
+                    backgroundColor: "#319795",
+                    scaleX: scrollYProgress
+                }}
+            /> */}
             <Section>
                 <TopProfile />
             </Section>
@@ -59,57 +74,6 @@ export const Home: FC = memo(() => {
                     </TabPanels>
                 </Tabs>
             </Section>
-
-            {/* <Section 
-                title='HISTORY'
-                description='経歴。'
-            >
-                <Box my={5}>
-                    <Heading as='h3' fontSize='2xl'>2019.03 大学卒業</Heading>
-                    <Text>自然言語処理を専攻。<br />卒業研究ではTwitter上の会話文を学習データとした言語生成モデルに関する研究をおこなう。</Text>
-                </Box>
-
-                <Box my={5}>
-                    <Heading as='h3' fontSize='2xl'>2019.04 日系ITベンダー入社</Heading>
-                    <Heading as='h3' fontSize='2xl'>2019.09 医療従事者向け会員サイト構築PJ</Heading>
-                    <Text>メンバーとしてCMSを使ったサイト構築の案件に携わる。<br />
-                    2年後のリリースまで参画。<br />
-                    主にサイト全体の共通パーツやログイン機能、動画配信機能の設計・実装を担当した。</Text>
-                </Box>
-
-                <Box my={5}>
-                    <Heading as='h3' fontSize='2xl'>2021.10 CMSバージョンアップPJ</Heading>
-                    <Text>Drupal8からDrupal9へのバージョンアップを実施するPJに参画。<br />
-                    事前調査や検証、手順の確立や組織の発信をおこなうことができた。<br />
-                    わずか3ヶ月で10サイトで実行することができた。</Text>
-                </Box>
-
-                <Box my={5}>
-                    <Heading as='h3' fontSize='2xl'>2022.01 コーポレートサイト運用PJ</Heading>
-                    <Text>クライアントの運営するコーポレートサイト全10サイトの運用保守に参画。<br />
-                    マーケティング系のツールを使ってアクセス解析やコンバージョンアップへの施策提案・実行を主に実施。<br />
-                    サブPJとして実施した新規サイト構築ではコストやスケジュール、品質、タスクなどPJ全体のマネジメントもおこなった。</Text>
-                </Box>
-
-                <Box my={5}>
-                    <Heading as='h3' fontSize='2xl'>2022.01 教育サービスTA</Heading>
-                    <Text>教育サービスのTA（ティーチングアシスタント）業務を開始。<br />
-                    内容はGitやAWSに関する講座のQA対応や補足資料作成。<br />
-                    初めての方でもわかりやすい言葉や文章を使うことを心がけています。</Text>
-                </Box>
-
-                <Box my={5}>
-                    <Heading as='h3' fontSize='2xl'>2023.04 MR向け製品情報サイト構築PJ</Heading>
-                    <Text>製薬企業のMRが業務で使うサイトの構築に参画。<br />
-                    主に設計や実装がメインだったが品質管理や課題管理、技術調査も実施。</Text>
-                </Box>
-
-                <Box my={5}>
-                    <Heading as='h3' fontSize='2xl'>2023.12〜現在 データ分析ツール構築PJ</Heading>
-                    <Text>製薬企業のMRが業務で使うツールの構築に参画。<br />
-                    要件定義やメンバーの成果物レビュー、課題や品質管理など上流の業務がメイン</Text>
-                </Box>            
-            </Section> */}
 
             <Section 
                 title='CONTACT'
